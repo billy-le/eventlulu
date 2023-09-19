037fa89f098b1fcbb6b34e2a54780f4d9a6a25d4
Billy Le <hi@billyle.dev>
2023-09-18 19:02:07 -0700

    properly set env for prisma seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -22,6 +22,8 @@ const {
   location,
   lorem,
 } = faker;
+import { parseArgs } from "node:util";
+
 const prisma = new PrismaClient();
 
 if (!process.env.DEFAULT_PASSWORD) {
@@ -30,6 +32,12 @@ if (!process.env.DEFAULT_PASSWORD) {
 }
 
 async function main() {
+  const {
+    values: { environment, seedCount },
+  } = parseArgs({
+    options: { environment: { type: "string" }, seedCount: { type: "string" } },
+  });
+
   const corporateEvents: Array<{
     id: string;
     name: string;
@@ -127,178 +135,189 @@ async function main() {
     rateTypesArray.push(type);
   }
 
-  if (process.env.NODE_ENV === "development") {
-    let seedCount = 0;
+  switch (environment) {
+    case "development":
+      /** data for your development */
+      let seed = 0;
 
-    if (process.argv[2]) {
-      const count = parseInt(process.argv[2], 10);
-      if (Number.isInteger(count) && Math.sign(count) === 1) {
-        seedCount = count;
+      if (seedCount) {
+        const count = parseInt(seedCount ?? "0", 10);
+        if (Number.isInteger(count) && Math.sign(count) === 1) {
+          seed = count;
+        }
       }
-    }
 
-    const user = await prisma.user.upsert({
-      where: {
-        email: "user@example.com",
-      },
-      update: {},
-      create: {
-        email: "user@example.com",
-        name: "user",
-        password: process.env.DEFAULT_PASSWORD!,
-        role: "salesManager",
-        phoneNumber: "123-123-1234",
-      },
-    });
-
-    const now = new Date();
-
-    for (let i = 0; i < seedCount; i++) {
-      const isCorporate = datatype.boolean();
-      const isLiveIn = datatype.boolean();
-      const eventLengthInDays = number.int({ min: 1, max: 5 });
-      const startDate = date.soon();
-      const endDate = datefns.addDays(startDate, eventLengthInDays - 1);
-      const contactFirstName = person.firstName();
-      const contactLastName = person.lastName();
-
-      const contact = await prisma.contact.create({
-        data: {
-          email: internet.email({
-            firstName: contactFirstName,
-            lastName: contactLastName,
-            provider: "example.com",
-          }),
-          firstName: contactFirstName,
-          lastName: contactLastName,
-          phoneNumber: phone.number(),
-          title: person.prefix(person.sexType()),
+      const user = await prisma.user.upsert({
+        where: {
+          email: "user@example.com",
+        },
+        update: {},
+        create: {
+          email: "user@example.com",
+          name: "user",
+          password: process.env.DEFAULT_PASSWORD!,
+          role: "salesManager",
+          phoneNumber: "123-123-1234",
         },
       });
 
-      let company;
-      if (isCorporate) {
-        company = await prisma.organization.create({
+      const now = new Date();
+
+      for (let i = 0; i < seed; i++) {
+        const isCorporate = datatype.boolean();
+        const isLiveIn = datatype.boolean();
+        const eventLengthInDays = number.int({ min: 1, max: 5 });
+        const startDate = date.soon();
+        const endDate = datefns.addDays(startDate, eventLengthInDays - 1);
+        const contactFirstName = person.firstName();
+        const contactLastName = person.lastName();
+
+        const contact = await prisma.contact.create({
           data: {
-            name: fakerCompany.name(),
+            email: internet.email({
+              firstName: contactFirstName,
+              lastName: contactLastName,
+              provider: "example.com",
+            }),
+            firstName: contactFirstName,
+            lastName: contactLastName,
             phoneNumber: phone.number(),
-            address1: location.streetAddress(),
-            address2: location.secondaryAddress(),
+            title: person.prefix(person.sexType()),
           },
         });
-      }
 
-      const eventType = isCorporate
-        ? helpers.arrayElement(corporateEvents)
-        : helpers.arrayElement(socialFunctionEvents);
-
-      try {
-        let eventDetails = [];
-        let i = 0;
-        for (const _ of Array(eventLengthInDays)) {
-          const randomDate = date.anytime();
-          const startTime = datefns.format(randomDate, "HH:MM");
-          const randomEndDate = datefns.addHours(
-            randomDate,
-            number.int({ min: 1, max: 8 })
-          );
-          const endTime = datefns.format(randomEndDate, "HH:MM");
-
-          const detail = await prisma.eventDetails.create({
+        let company;
+        if (isCorporate) {
+          company = await prisma.organization.create({
             data: {
-              date: datefns.addDays(startDate, i),
-              pax: number.int({ max: 200 }),
-              roomSetupId: helpers.arrayElement(roomSetupsArray).id,
-              functionRoomId: helpers.arrayElement(functionRoomsArray).id,
-              remarks: lorem.sentence(),
-              rate: number.int({ max: 500_000 }),
-              rateTypeId: helpers.arrayElement(rateTypesArray).id,
-              mealReqs: {
-                connect: helpers.arrayElements(mealReqsArray, {
-                  min: 1,
-                  max: 3,
-                }),
-              },
-              startTime,
-              endTime,
+              name: fakerCompany.name(),
+              phoneNumber: phone.number(),
+              address1: location.streetAddress(),
+              address2: location.secondaryAddress(),
             },
           });
-
-          eventDetails.push(detail);
-          i++;
         }
 
-        await prisma.leadForm.create({
-          data: {
-            dateReceived: date.past({ refDate: now, years: 0.2 }),
-            isCorporate,
-            isLiveIn,
-            eventLengthInDays,
-            startDate,
-            endDate,
-            status: helpers.arrayElement(Object.values(EventStatus)),
-            roomsBudget: isLiveIn ? number.int({ max: 50_000 }) : 0,
-            banquetsBudget: number.int({ max: 50_000 }),
-            contactId: contact.id,
-            companyId: company?.id ?? null,
-            salesAccountManagerId: user.id,
-            leadTypeId: helpers.arrayElement(leadTypesArray).id,
-            eventTypeId: eventType?.id,
-            eventTypeOther: !eventType ? word.verb() : null,
-            lastDateSent: helpers.maybe(() => date.recent({ refDate: now }), {
-              probability: 0.5,
-            }),
-            onSiteDate: helpers.maybe(
-              () =>
-                date.soon({
-                  refDate: now,
-                  days: number.int({ min: 1, max: 15 }),
-                }),
-              { probability: 0.7 }
-            ),
-            roomType: isLiveIn ? word.adjective() : null,
-            roomTotal: isLiveIn ? number.int({ max: 200 }) : null,
-            roomArrivalDate: isLiveIn ? startDate : null,
-            roomDepartureDate: isLiveIn ? endDate : null,
-            otherHotelConsiderations: helpers.maybe(
-              () => {
-                return Array(number.int({ min: 1, max: 4 }))
-                  .fill(null)
-                  .map((_) => `${word.noun()} hotel`)
-                  .join(", ");
+        const eventType = isCorporate
+          ? helpers.arrayElement(corporateEvents)
+          : helpers.arrayElement(socialFunctionEvents);
+
+        try {
+          let eventDetails = [];
+          let i = 0;
+          for (const _ of Array(eventLengthInDays)) {
+            const randomDate = date.anytime();
+            const startTime = datefns.format(randomDate, "HH:MM");
+            const randomEndDate = datefns.addHours(
+              randomDate,
+              number.int({ min: 1, max: 8 })
+            );
+            const endTime = datefns.format(randomEndDate, "HH:MM");
+
+            const detail = await prisma.eventDetails.create({
+              data: {
+                date: datefns.addDays(startDate, i),
+                pax: number.int({ max: 200 }),
+                roomSetupId: helpers.arrayElement(roomSetupsArray).id,
+                functionRoomId: helpers.arrayElement(functionRoomsArray).id,
+                remarks: lorem.sentence(),
+                rate: number.int({ max: 500_000 }),
+                rateTypeId: helpers.arrayElement(rateTypesArray).id,
+                mealReqs: {
+                  connect: helpers.arrayElements(mealReqsArray, {
+                    min: 1,
+                    max: 3,
+                  }),
+                },
+                startTime,
+                endTime,
               },
-              { probability: 0.3 }
-            ),
-            activities: {
-              createMany: {
-                data: Array(number.int({ max: 4 }))
-                  .fill(null)
-                  .map((_, index) => ({
-                    date: datefns.addDays(startDate, index),
-                    updatedById: user.id,
-                    clientFeedback: lorem.sentences(),
-                    nextTraceDate: helpers.maybe(
-                      () =>
-                        date.soon({
-                          refDate: now,
-                          days: number.int({ min: 1, max: 15 }),
-                        }),
-                      {
-                        probability: 0.2,
-                      }
-                    ),
-                  })),
+            });
+
+            eventDetails.push(detail);
+            i++;
+          }
+
+          await prisma.leadForm.create({
+            data: {
+              dateReceived: date.past({ refDate: now, years: 0.2 }),
+              isCorporate,
+              isLiveIn,
+              eventLengthInDays,
+              startDate,
+              endDate,
+              status: helpers.arrayElement(Object.values(EventStatus)),
+              roomsBudget: isLiveIn ? number.int({ max: 50_000 }) : 0,
+              banquetsBudget: number.int({ max: 50_000 }),
+              contactId: contact.id,
+              companyId: company?.id ?? null,
+              salesAccountManagerId: user.id,
+              leadTypeId: helpers.arrayElement(leadTypesArray).id,
+              eventTypeId: eventType?.id,
+              eventTypeOther: !eventType ? word.verb() : null,
+              lastDateSent: helpers.maybe(() => date.recent({ refDate: now }), {
+                probability: 0.5,
+              }),
+              onSiteDate: helpers.maybe(
+                () =>
+                  date.soon({
+                    refDate: now,
+                    days: number.int({ min: 1, max: 15 }),
+                  }),
+                { probability: 0.7 }
+              ),
+              roomType: isLiveIn ? word.adjective() : null,
+              roomTotal: isLiveIn ? number.int({ max: 200 }) : null,
+              roomArrivalDate: isLiveIn ? startDate : null,
+              roomDepartureDate: isLiveIn ? endDate : null,
+              otherHotelConsiderations: helpers.maybe(
+                () => {
+                  return Array(number.int({ min: 1, max: 4 }))
+                    .fill(null)
+                    .map((_) => `${word.noun()} hotel`)
+                    .join(", ");
+                },
+                { probability: 0.3 }
+              ),
+              activities: {
+                createMany: {
+                  data: Array(number.int({ max: 4 }))
+                    .fill(null)
+                    .map((_, index) => ({
+                      date: datefns.addDays(startDate, index),
+                      updatedById: user.id,
+                      clientFeedback: lorem.sentences(),
+                      nextTraceDate: helpers.maybe(
+                        () =>
+                          date.soon({
+                            refDate: now,
+                            days: number.int({ min: 1, max: 15 }),
+                          }),
+                        {
+                          probability: 0.2,
+                        }
+                      ),
+                    })),
+                },
+              },
+              eventDetails: {
+                connect: eventDetails,
               },
             },
-            eventDetails: {
-              connect: eventDetails,
-            },
-          },
-        });
-      } catch (err) {
-        console.log(err);
+          });
+        } catch (err) {
+          console.log(err);
+        }
       }
-    }
+      break;
+
+    case "test":
+      /** data for your test environment */
+
+      break;
+
+    default:
+      break;
   }
 }
 
