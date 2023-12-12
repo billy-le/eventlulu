b27bb3f7fbe9ef193746c4303b0dcf66181ea62c
Billy Le <hi@billyle.dev>
2023-12-12 10:09:16 +0800

    update ui for event list

--- src/ui/OverviewCalendar.tsx
+++ src/ui/OverviewCalendar.tsx
@@ -19,9 +19,91 @@ import {
   isToday,
   isWithinInterval,
   formatDistanceToNow,
+  isBefore,
 } from "date-fns";
 import type { RouterOutputs } from "~/utils/api";
 import type { DateRangeData } from "./DateRangeMode";
+
+function renderEventList({
+  day,
+  leads,
+}: {
+  day: Date;
+  leads: RouterOutputs["leads"]["getLeads"];
+}) {
+  return leads
+    .sort(
+      (a, b) =>
+        a.eventDetails[0]?.startTime?.localeCompare(
+          b.eventDetails[0]?.startTime ?? ""
+        ) ?? -1
+    )
+    .map((event) => {
+      const eventDetail = event.eventDetails.find((d) =>
+        isSameDay(d.date, day)
+      );
+      const start = new Date(eventDetail?.date!);
+      let end: Date | undefined = new Date(eventDetail?.date!);
+
+      start.setSeconds(0);
+      start.setMilliseconds(0);
+      end.setSeconds(0);
+      end.setMilliseconds(0);
+      const [shours, sminutes] = eventDetail?.startTime?.split(":") ?? [];
+      const [ehours, eminutes] = eventDetail?.endTime?.split(":") ?? [];
+      if (shours) {
+        start.setHours(parseInt(shours, 10));
+      }
+      if (sminutes) {
+        start.setMinutes(parseInt(sminutes, 10));
+      }
+      if (ehours) {
+        end.setHours(parseInt(ehours, 10));
+      }
+      if (eminutes) {
+        end.setMinutes(parseInt(eminutes), 10);
+      }
+      if (isAfter(start, end)) {
+        end = addDays(end, 1);
+      }
+
+      return (
+        <li
+          key={event.id}
+          className="flex flex-col items-center justify-center p-2 text-xs"
+        >
+          <p className="flex items-center gap-1">
+            <span>{event.contact?.firstName}</span>
+            {event.status === "confirmed" &&
+            isWithinInterval(new Date(), {
+              start,
+              end,
+            }) ? (
+              <div className="relative ml-2 grid place-items-center">
+                <div className="absolute h-2 w-2 rounded-full bg-green-400"></div>
+                <div className="absolute h-2 w-2 animate-ping rounded-full bg-green-600"></div>
+              </div>
+            ) : (
+              isBefore(new Date(), start) && (
+                <span className="text-gray-400">
+                  -{" "}
+                  {formatDistanceToNow(start, {
+                    addSuffix: true,
+                  })}
+                </span>
+              )
+            )}
+          </p>
+          {eventDetail?.functionRoom && (
+            <p className="capitalize">{eventDetail.functionRoom.name}</p>
+          )}
+          <p>
+            {dateFormat(start, "hh:mm a")} - {dateFormat(end, "hh:mm a")}
+          </p>
+        </li>
+      );
+    });
+}
 
 export function OverviewCalendar({
   leads,
@@ -71,10 +153,10 @@ export function OverviewCalendar({
         Day: (day) => {
           const today = isToday(day.date);
           const insideDay = isSameMonth(day.date, internalDate);
-          const tentativeCount = tentatives.filter((t) =>
+          const tentativeEvents = tentatives.filter((t) =>
             isSameDay(t.startDate, day.date)
           );
-          const confirmedCount = confirms.filter((c) =>
+          const confirmedEvents = confirms.filter((c) =>
             isSameDay(c.startDate, day.date)
           );
 
@@ -86,105 +168,47 @@ export function OverviewCalendar({
             >
               <div className="text-right">{dateFormat(day.date, "d")}</div>
               <div className="absolute bottom-1 flex gap-2">
-                {confirmedCount.length > 0 && (
+                {confirmedEvents.length > 0 && (
                   <Popover>
                     <PopoverTrigger>
                       <div className="grid h-6 w-6 place-items-center rounded-full bg-green-200 text-xs shadow-sm">
                         <span className="sr-only">view confirmed events</span>
-                        {confirmedCount.length}
+                        {confirmedEvents.length}
                       </div>
                     </PopoverTrigger>
                     <PopoverPortal>
-                      <PopoverContent className="max-h-60 w-60 overflow-y-auto rounded-md bg-white p-2 shadow-md">
-                        <ul className="space-y-2">
-                          {confirmedCount.map((confirmed) => {
-                            const eventDetail = confirmed.eventDetails.find(
-                              (d) => isSameDay(d.date, day.date)
-                            );
-                            const start = new Date(eventDetail?.date);
-                            let end: Date | undefined = new Date(
-                              eventDetail?.date
-                            );
-
-                            start.setSeconds(0);
-                            start.setMilliseconds(0);
-                            end.setSeconds(0);
-                            end.setMilliseconds(0);
-                            const [shours, sminutes] =
-                              eventDetail?.startTime?.split(":") ?? [];
-                            const [ehours, eminutes] =
-                              eventDetail?.endTime?.split(":") ?? [];
-                            if (shours) {
-                              start.setHours(parseInt(shours, 10));
-                            }
-                            if (sminutes) {
-                              start.setMinutes(parseInt(sminutes, 10));
-                            }
-                            if (ehours) {
-                              end.setHours(parseInt(ehours, 10));
-                            }
-                            if (eminutes) {
-                              end.setMinutes(parseInt(eminutes), 10);
-                            }
-                            if (isAfter(start, end)) {
-                              end = addDays(end, 1);
-                            }
-
-                            return (
-                              <li key={confirmed.id} className="text-xs">
-                                <p className="flex items-center gap-1">
-                                  <span>{confirmed.contact?.firstName}</span>
-                                  {isWithinInterval(new Date(), {
-                                    start,
-                                    end,
-                                  }) ? (
-                                    <div className="relative ml-2 grid place-items-center">
-                                      <div className="absolute h-2 w-2 rounded-full bg-green-400"></div>
-                                      <div className="absolute h-2 w-2 animate-ping rounded-full bg-green-600"></div>
-                                    </div>
-                                  ) : (
-                                    <span className="text-gray-400">
-                                      -{" "}
-                                      {formatDistanceToNow(start, {
-                                        addSuffix: true,
-                                      })}
-                                    </span>
-                                  )}
-                                </p>
-                                {eventDetail?.functionRoom && (
-                                  <p className="capitalize">
-                                    {eventDetail.functionRoom.name}
-                                  </p>
-                                )}
-                                <p>
-                                  {dateFormat(start, "hh:mm a")} -{" "}
-                                  {dateFormat(end, "hh:mm a")}
-                                </p>
-                              </li>
-                            );
+                      <PopoverContent className="max-h-60 w-48 overflow-y-auto rounded-md bg-white shadow-md">
+                        <ul className="divide-y-slate-200 divide-y">
+                          {isAfter(new Date(), day.date) && (
+                            <li className="p-2 text-center text-sm text-slate-400">
+                              {formatDistanceToNow(day.date, {
+                                addSuffix: true,
+                              })}
+                            </li>
+                          )}
+                          {renderEventList({
+                            leads: confirmedEvents,
+                            day: day.date,
                           })}
                         </ul>
                       </PopoverContent>
                     </PopoverPortal>
                   </Popover>
                 )}
-                {tentativeCount.length > 0 && (
+                {tentativeEvents.length > 0 && (
                   <Popover>
                     <PopoverTrigger>
                       <div className="grid h-6 w-6 place-items-center rounded-full bg-yellow-200 text-xs shadow-sm">
                         <span className="sr-only">view tentative events</span>
-                        {tentativeCount.length}
+                        {tentativeEvents.length}
                       </div>
                     </PopoverTrigger>
                     <PopoverPortal>
-                      <PopoverContent className="max-h-60 w-40 overflow-y-auto rounded-md bg-white p-2 shadow-md">
-                        <ul>
-                          {tentativeCount.map((tentative) => {
-                            return (
-                              <li key={tentative.id} className="text-xs">
-                                <span>{tentative.contact?.firstName}</span>
-                              </li>
-                            );
+                      <PopoverContent className="max-h-60 w-48 overflow-y-auto rounded-md bg-white shadow-md">
+                        <ul className="divide-y-slate-200 divide-y">
+                          {renderEventList({
+                            leads: tentativeEvents,
+                            day: day.date,
                           })}
                         </ul>
                       </PopoverContent>
